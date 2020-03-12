a7c3ef2de39eac7e5b5a310164930c8de1bb3174
Sergey Grishchenko <s.i.grishchenko@gmail.com>
2020-03-12 12:36:50 +0200

    docz static is cleared

--- docz/styles-9454499d47ea7ec86a46.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{oKgo:function(o,n,w){}}]);
-//# sourceMappingURL=styles-9454499d47ea7ec86a46.js.map
--- docz/styles-ba763825ee89fcab79c7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{oKgo:function(o,n,w){}}]);
-//# sourceMappingURL=styles-ba763825ee89fcab79c7.js.map
