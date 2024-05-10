c5c8d279e5558ce21022d6bf96a3f5ced32674b9
Alex Khizhnyi <mendler88@gmail.com>
2024-05-10 13:38:14 +0300

    exclude react and react-dom from build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,4 @@
-import {defineConfig, Options} from 'tsup';
-import type {ExecOptions} from 'shelljs';
-import sh from 'shelljs';
-
-function execAsync(cmd: string, opts: ExecOptions = {}) {
-  return new Promise(function (resolve, reject) {
-    // Execute the command, reject if we exit non-zero (i.e. error)
-    sh.exec(cmd, opts, function (code, stdout, stderr) {
-      if (code !== 0) return reject(new Error(stderr));
-      return resolve(stdout);
-    });
-  });
-}
+import { defineConfig, Options } from 'tsup';
 
 export default defineConfig(options => {
   const commonOptions: Partial<Options> = {
@@ -26,41 +14,18 @@ export default defineConfig(options => {
     {
       ...commonOptions,
       format: ['esm'],
+      external: ['react', 'react-dom'],
       target: 'es2019',
-      outExtension: () => ({js: '.mjs'}),
+      outExtension: () => ({ js: '.mjs' }),
       dts: true,
       clean: true,
-    },
-
-    // Support Webpack 4 by pointing `"module"` to a file with a `.js` extension
-    // and optional chaining compiled away
-    {
-      ...commonOptions,
-      entry: {
-        'reselectUtils.legacy-esm': 'src/index.ts',
-      },
-      format: ['esm'],
-      outExtension: () => ({js: '.js'}),
-      target: 'es2017',
-    },
-    // Browser-ready ESM, production + minified
-    {
-      ...commonOptions,
-      entry: {
-        'reselectUtils.browser': 'src/index.ts',
-      },
-      define: {
-        'process.env.NODE_ENV': JSON.stringify('production'),
-      },
-      format: ['esm'],
-      outExtension: () => ({js: '.mjs'}),
-      minify: true,
     },
     {
       ...commonOptions,
       format: 'cjs',
+      external: ['react', 'react-dom'],
       outDir: './dist/cjs/',
-      outExtension: () => ({js: '.cjs'}),
+      outExtension: () => ({ js: '.cjs' }),
     },
   ];
 });
