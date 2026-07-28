fce85e171a50b348207ce32ba7568f4fcba5386b
Alex K <mendler88@gmail.com>
2026-07-28 11:18:49 +0300

    refactor(cache): require keySelector to be a function in isCachedSelector

--- src/_helpers/isCachedSelector.ts
+++ src/_helpers/isCachedSelector.ts
@@ -1,5 +1,9 @@
 import { CachedSelector } from '../types';
 
 export const isCachedSelector = (selector: unknown): selector is CachedSelector => {
-  return selector instanceof Object && 'keySelector' in selector;
+  return (
+    selector instanceof Object &&
+    'keySelector' in selector &&
+    typeof (selector as CachedSelector).keySelector === 'function'
+  );
 };
